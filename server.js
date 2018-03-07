4a2d6ee1712781d3a52f13bc3da1bd1e985dfc2f
Edward Bao <Ebao@MacBook-Pro.local>
2018-03-07 12:57:24 -0600

    third try

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var path = require('path');
+var express = require('express');
 
-app.use(express.static(__dirname + '/dist/'));
-app.use('/src/js', express.static(__dirname + '/src/js/'));
+var app = express();
 
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
 
-app.listen(process.env.PORT || 8080);
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
